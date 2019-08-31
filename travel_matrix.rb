93dd4f5c84828d559289815fe0a731f913f86e52
Max Würfel <maxwuerfel24@gmail.com>
2019-08-31 16:39:46 +0200

    .env

--- travel_matrix.rb
+++ travel_matrix.rb
@@ -2,16 +2,18 @@ require 'matrix'
 require 'open-uri'
 require 'nokogiri'
 require 'json'
+require 'dotenv/load'
 require 'pry-byebug'
 
 class TravelMatrix
   def self.generate(sights, travel_mode)
     times = Matrix.build(sights.length, sights.length) { |row, col| { start: sights[row], end: sights[col] } }
+    key = ENV['GOOGLE_API_SERVER_KEY']
     times.each_with_index do |cell, row, col|
-      url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{cell[:start][0]},#{cell[:start][1]}&destinations=#{cell[:end][0]}%2C#{cell[:end][1]}&mode=#{travel_mode}&key=AIzaSyAnfB-f3jSliP1uK8vkBnYt56il_QX1VL0"
+      url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{cell[:start][0]},#{cell[:start][1]}&destinations=#{cell[:end][0]}%2C#{cell[:end][1]}&mode=#{travel_mode}&key=#{key}"
       serialized = open(url).read
       info = JSON.parse(serialized)
-      times[row, col] = info["rows"][0]["elements"][0]["duration"]["value"]
+      times[row, col] = info['rows'][0]['elements'][0]['duration']['value']
     end
   end
 end
